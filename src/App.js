98c73742cd6a4b8087186d443b81b1186c3504c6
SadiaSultana-Kumu <kumusadia02@gmail.com>
2022-06-18 21:28:54 +0600

    [ADD] react query devtools

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 function App() {
   const [page, setPage] = useState('Planets')
   return (
-    <div className="App">
-      <h1>Star wars Info</h1>
-      <Navbar setPage={setPage}/>
-      <div className="content">
-        {page === 'Planets' ? <Planets/> : <People/>}
+    <>
+      <div className="App">
+        <h1>Star wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === 'Planets' ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
